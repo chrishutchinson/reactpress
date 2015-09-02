6c5e4107b5fca2bb7eb5a9d9657564babbf279d3
Chris Hutchinson <chris_hutchinson@me.com>
2015-09-02 15:18:00 +0100

    Fix for slugness

--- src/components/WordpressPost.js
+++ src/components/WordpressPost.js
@@ -36,7 +36,7 @@ var WordpressPost = React.createClass({
         <div className="row WordpressPost">
           <div className="col-md-12">
             <h1>
-              <Link to="post" params={{postId: this.props.post.ID}}>{this.props.post.title}</Link>
+              <Link to="post" params={{postId: this.props.post.slug}}>{this.props.post.title}</Link>
               <small> By {this.props.post.author.name}</small>
             </h1>
             <hr />
